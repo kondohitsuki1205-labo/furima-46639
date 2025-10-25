ace50df62dbb8593b2b070ceba317c9279cd3114
kondohitsuki1205-labo <kondo.itsuki1205@gmail.com>
2025-10-25 20:44:22 +0900

    Finish user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
   def configure_permitted_parameters
-   added = %i[nickname last_name first_name last_name_kana first_name_kana birthday]
-   devise_parameter_sanitizer.permit(:sign_up,        keys: added)
-   devise_parameter_sanitizer.permit(:account_update, keys: added)
+    added = %i[
+      nickname last_name first_name last_name_kana first_name_kana birthday
+    ]
+    devise_parameter_sanitizer.permit(:sign_up,        keys: added)
+    devise_parameter_sanitizer.permit(:account_update, keys: added)
   end
 end
